d2f9eea898b02d97c876a05965680b664df5301e
Matthew Taylor <mewtaylor@gmail.com>
2020-04-16 09:59:27 -0400

    Gatsby Initialization

---
+++ src/pages/index.js
@@ -0,0 +1,23 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+import style from "./index.module.css"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hi people</h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <section className={style.feature}>
+      <h1>Using CSS Modules</h1>
+    </section>
+    <Link to="/page-2/">Go to page 2</Link>
+  </Layout>
+)
+
+export default IndexPage
