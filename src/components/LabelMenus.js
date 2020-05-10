6c46bc89280c5dd278a3ce2902a167b55b40a1df
Kemi Thomas <kemothom71@gmail.com>
2020-05-10 14:39:11 -0400

    added instructions for LabelMenus

--- src/components/LabelMenus.js
+++ src/components/LabelMenus.js
@@ -1,8 +1,14 @@
-import React from "react"
-
+import React, { useState } from "react"
+//what does the menu item
 const LabelMenus = props => {
+  const [MenuTitle, setText] = useState("UseCases and Alerts")
+  function clickMenu() {
+    //create a function that changes the header text to the
+    //create id from the props to change the text to the correct name
+    setText("")
+  }
   return (
-    <div className="Label-Menus">
+    <div className="Label-Menus" onClick={clickMenu}>
       <header className="section-name"></header>
       <span className="weighted-line">
         <p>Lorem Ipsum</p>
