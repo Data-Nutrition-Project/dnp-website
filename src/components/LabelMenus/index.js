2c0361fd17db99902087da334625bf5aba3536e8
Kemi Thomas <kemothom71@gmail.com>
2020-05-18 19:46:04 -0400

    highlight function highlights all divs instead of one

--- src/components/LabelMenus/index.js
+++ src/components/LabelMenus/index.js
@@ -9,16 +9,36 @@ class LabelMenus extends Component {
   constructor() {
     super()
     this.state = {
+      activeItem: -1,
+      showHighlight: false,
       titleList: ["Use Cases and Alerts", "Overview", "Maintenance"],
     }
-    this.highlightLabel = this.highlightLabel.bind(this)
+    // this.highlightLabel = this.highlightLabel.bind(this)
   }
 
-  highlightLabel(event) {
-    console.log(event.target)
-    event.target.element.class = ".highlightLabel"
-  }
+  //   highlightLabel(event) {
+  //     console.log(event.target)
+  //  event.
+  //   }
 
+  highlight = () => {
+    this.setState(prevState => ({
+      showHighlight: !prevState.showHighlight,
+    }))
+  }
+  handleItemClick(index) {
+    this.setState({
+      activeItem: index,
+    })
+  }
+  myFunction() {
+    var x = document.getElementsByClassName("highlightLabel")
+    if (x.style.display === "none") {
+      x.style.display = "block"
+    } else {
+      x.style.display = "none"
+    }
+  }
   //   //create a function that changes the header text to the
   //   //create id from the props to change the text to the correct name
   // onClickFunction=this.props.dispatch(item.key)
@@ -26,17 +46,36 @@ class LabelMenus extends Component {
     return (
       <div>
         <div className={styles.sectionName}>
-          {this.state.titleList.map(title => {
+          {this.state.titleList.map((title, index) => {
             return (
               <div
                 // style={{ background: color }}
-                key={title.key}
+                key={index}
                 // onClick={() => {
                 // setColor("black")
                 // }}
-                className={styles.labelMenus}
+                className={
+                  this.state.activeItem === index
+                    ? `  ${styles.highlightLabel}`
+                    : styles.labelMenus
+                }
+                onClick={this.highlight}
+                // onClick={this.myFunction.bind(this)}
+                // onClick={this.handleItemClick.bind(this, index)}
                 // onClick={this.highlightLabel}
               >
+                <span
+                  key={index}
+                  className={`${
+                    this.state.showHighlight ? styles.highlightLabel : ""
+                  }`}
+                ></span>
+                <div
+                  key={index}
+                  style={{
+                    display: this.state.showHighlight ? "block" : "none",
+                  }}
+                ></div>
                 {/* <MenuItem name={title.name} key={title.key} /> */}
                 <span className={styles.menuTitle}>{title}</span>
                 <div className={styles.flexbox}>
