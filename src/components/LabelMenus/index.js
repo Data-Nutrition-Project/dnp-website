d081366e7cbdd2c76f727d49575704a28f96a111
Kemi Thomas <kemothom71@gmail.com>
2020-05-19 15:31:08 -0400

    created working highlight function

--- src/components/LabelMenus/index.js
+++ src/components/LabelMenus/index.js
@@ -1,89 +1,46 @@
 import styles from "./styles.module.css"
 import React, { Component } from "react"
 import { connect } from "react-redux"
-import { toggleHighlight } from "../../store/labelMenu"
-//TURN INTO A CLASS COMPONENT
-//what does the menu item
 
 class LabelMenus extends Component {
   constructor() {
     super()
     this.state = {
-      activeItem: -1,
-      showHighlight: false,
       titleList: ["Use Cases and Alerts", "Overview", "Maintenance"],
     }
-    // this.highlightLabel = this.highlightLabel.bind(this)
   }
 
-  //   highlightLabel(event) {
-  //     console.log(event.target)
-  //  event.
-  //   }
-
-  highlight = () => {
-    this.setState(prevState => ({
-      showHighlight: !prevState.showHighlight,
-    }))
-  }
-  handleItemClick(index) {
+  selectItem = item =>
     this.setState({
-      activeItem: index,
+      selectedItem: item,
+      showHighlight: false,
     })
-  }
-  myFunction() {
-    var x = document.getElementsByClassName("highlightLabel")
-    if (x.style.display === "none") {
-      x.style.display = "block"
-    } else {
-      x.style.display = "none"
-    }
-  }
-  //   //create a function that changes the header text to the
-  //   //create id from the props to change the text to the correct name
-  // onClickFunction=this.props.dispatch(item.key)
   render() {
     return (
       <div>
-        <div className={styles.sectionName}>
-          {this.state.titleList.map((title, index) => {
+        <div style={{ display: this.state.titleList }}>
+          {this.state.titleList.map(title => {
             return (
               <div
-                // style={{ background: color }}
-                key={index}
-                // onClick={() => {
-                // setColor("black")
-                // }}
+                key={title.id}
+                onClick={() => this.selectItem(title)}
                 className={
-                  this.state.activeItem === index
-                    ? `  ${styles.highlightLabel}`
+                  this.state.selectedItem === title
+                    ? `${styles.highlightLabel} ${styles.labelMenus}`
                     : styles.labelMenus
                 }
-                onClick={this.highlight}
-                // onClick={this.myFunction.bind(this)}
-                // onClick={this.handleItemClick.bind(this, index)}
-                // onClick={this.highlightLabel}
               >
-                <span
-                  key={index}
-                  className={`${
-                    this.state.showHighlight ? styles.highlightLabel : ""
-                  }`}
-                ></span>
                 <div
-                  key={index}
+                  key={title.id}
                   style={{
                     display: this.state.showHighlight ? "block" : "none",
                   }}
                 ></div>
-                {/* <MenuItem name={title.name} key={title.key} /> */}
                 <span className={styles.menuTitle}>{title}</span>
                 <div className={styles.flexbox}>
                   <p className={styles.lorem}>Lorem</p>
                   <span className={styles.weightedLine}></span>
                 </div>
-                {/* <button onClick={this.highlightLabel}>HI THEREEEE</button> */}
-
                 <p className={styles.menuParagraph}>
                   Qui ut occaecat exercitation amet mollit excepteur do aliquip
                   et sunt sit aliqua consectetur. Sit minim voluptate aliqua id
--- src/components/MenuItem/index.js
+++ src/components/MenuItem/index.js
@@ -2,27 +2,14 @@ import React from "React"
 import { toggleHighlight } from "../../store/labelMenu"
 import styles from "./styles.module.css"
 const MenuItem = props => {
-  //function should be from the redux store to setState to the name of the menuItem
-
-  //take in variable highlighted and to add a className
-  //then handle the css
-  //each name should have a key use the key in the redux store
   //add PropTypes to check for bugs ,and default values
-  const highlightMenu = ({ highlight, dispatch }) => {
-    const newColor =
-      props.color === "" ? '<div className="div-highlight"></div>' : ""
-    props.updateBackground(newColor)
-  }
   return (
-    // props.highlighted
-    //props.onClick() rename
     <div
       className={styles.regular}
       onClick={() => {
         props.dispatch(toggleHighlight(!highlight))
       }}
     >
-      {/* onClick props  */}
       <h1>{props.name}</h1>
       {highlight ? (
         <div className={styles.highlightLabel} />
@@ -30,7 +17,6 @@ const MenuItem = props => {
         <div className={styles.regular} />
       )}
       {props.key}
-      {/* key  */}
     </div>
   )
 }
