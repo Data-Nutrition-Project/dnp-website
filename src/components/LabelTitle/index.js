0417732c01e97714ae8fcb2df506ea53f7e4ef78
Kemi Thomas <kemothom71@gmail.com>
2020-05-20 19:14:09 -0400

    created styling for label title

--- src/components/LabelTitle/index.js
+++ src/components/LabelTitle/index.js
@@ -8,16 +8,23 @@ const LabelTitle = props => {
       <h1 className={styles.datasetLabelHeader}>
         College Scoreboard Dataset 2020
       </h1>
-
+      <div className={styles.link}>
       <h2 className={styles.datasetNameLink}></h2>
-
+      </div>
+      <div className={styles.link}>
+      <a className={styles.datasetBoldLink} href="#">
+          <img src="/linkimg.png" alt="" id="datasetLink" />
+        </a>
+        </div>
       {/* {props.orginLink} */}
-      <span className="link">
+      <div className={styles.link}>
         <p className={styles.datasetOriginLink}>US Department of Education</p>
+             </div>
+             <div className={styles.link}>
         <a className={styles.datasetLink} href="#">
           <img src="/linkimg.png" alt="" id="datasetLink" />
         </a>
-      </span>
+    </div>
       <span className={styles.weightedLine}> </span>
     </div>
   )
