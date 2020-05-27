11e5f21eda134aa5785bf0b46f9c4ff0b307a4ee
Kemi Thomas <kemothom71@gmail.com>
2020-05-27 19:09:33 -0400

    created Label Wrapper to wrap components into one

---
+++ src/components/LabelWrapper/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+import styles from "./styles.module.css"
+import LabelMenus from "../LabelMenus/index"
+const LabelWrapper = () => {
+  return (
+    <div className={styles.labelWrapper}>
+      <LabelMenus />
+    </div>
+  )
+}
+
+export default LabelWrapper
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -2,12 +2,11 @@ import React from "react"
 import { Link } from "gatsby"
 import { Button } from "react-bootstrap"
 import LabelMenus from "../components/LabelMenus/index.js"
+import LabelWrapper from "../components/LabelWrapper/index.js"
 import LabelTitle from "../components/LabelTitle/index.js"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
-  <LabelMenus />
-)
+const SecondPage = () => <LabelWrapper />
 
 export default SecondPage
