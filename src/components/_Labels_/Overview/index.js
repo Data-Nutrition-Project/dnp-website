3c5fd8d1f25df05dc734e23b5783ea9b5688af07
Kemi Thomas <kemothom71@gmail.com>
2020-06-10 22:27:30 -0400

    started styling

--- src/components/_Labels_/Overview/index.js
+++ src/components/_Labels_/Overview/index.js
@@ -1,9 +1,32 @@
-import React from "react"
+import React, { Component } from "react"
 import SectionBase from "../../SectionBase/index.js"
-const Overview = () => (
-  <SectionBase>
-    <h1>Overview</h1>
-  </SectionBase>
-)
+import { render } from "enzyme"
+import styles from "./styles.module.css"
+class Overview extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+  render() {
+    return (
+      <SectionBase>
+        <div clasName={styles.flexbox}>
+          <h1 className={styles.overviewTitle}>Overview</h1>
+          <span className={styles.overviewUnderline}></span>
+        </div>
+        <h2 className={styles.overviewSubHeader}>About</h2>
+        <p className={styles.overviewParagraph}>dfsdfsdfdsf</p>
+        <span className={styles.overviewUnderline}></span>
+        <h2 className={styles.overviewSubHeader}>Motivation</h2>
+        <p className={styles.overviewParagraph}>dfsdfsdfdsf</p>
+        <span className={styles.overviewUnderline}></span>
+        <ol className={styles.overviewList}>
+          <li>sfsdfdsf</li>
+          <li>sdfdsfdf</li>
+        </ol>
+      </SectionBase>
+    )
+  }
+}
 
 export default Overview
