9b84b3d810be2fb1c07a527e43d4c1693de72f1e
Matthew Taylor <mewtaylor@gmail.com>
2020-06-15 21:21:50 -0400

    Fix lint warnings
    Most of these warnings related to ARIA standards, and are necessary for accessibility.
    
    This also does a few other cleanup things, such as:
    - standardizes indents to 4 spaces
    - standardizes import statement conventions (gatsby imports, then library imports, then file imports)

--- src/components/LabelTitle/index.js
+++ src/components/LabelTitle/index.js
@@ -1,49 +1,51 @@
 import React from "react"
+
 import PropTypes from 'prop-types'
+
 import styles from "./styles.module.css"
 
 const LabelTitle = props => {
-  return (
-    <div className={styles.labelTitle}>
-      <h1 className={styles.headerTitle}>Data Nutrition Label</h1>
-      <div className={styles.pageSubHeaderRow}>
-        <h2 className={styles.datasetLabelHeader}>
-          {props.datasetName}
-        </h2>
-        <a className={styles.iconLink} href={props.datasetNameLink}>
-          <img
-            className={styles.iconLinkImg}
-            src={require("../../images/linkimg.png")}
-            alt="icon for link"
-            id="datasetLink"
-          />
-        </a>
-      </div>
-      <div className={styles.link}>
-        <p className={styles.datasetOriginLink}>{props.datasetOrg}</p>
-      </div>
-      <div className={styles.link}>
-        <a className={styles.datasetLink} href={props.datasetOrgLink}>
-          <img src="/linkimg.png" alt="" id="datasetLink" />
-        </a>
-      </div>
-      <span className={styles.weightedLine}> </span>
-    </div>
-  )
+    return (
+        <div className={styles.labelTitle}>
+            <h1 className={styles.headerTitle}>Data Nutrition Label</h1>
+            <div className={styles.pageSubHeaderRow}>
+                <h2 className={styles.datasetLabelHeader}>
+                    {props.datasetName}
+                </h2>
+                <a className={styles.iconLink} href={props.datasetNameLink}>
+                    <img
+                        className={styles.iconLinkImg}
+                        src={require("../../images/linkimg.png")}
+                        alt="icon for link"
+                        id="datasetLink"
+                    />
+                </a>
+            </div>
+            <div className={styles.link}>
+                <p className={styles.datasetOriginLink}>{props.datasetOrg}</p>
+            </div>
+            <div className={styles.link}>
+                <a className={styles.datasetLink} href={props.datasetOrgLink}>
+                    <img src="/linkimg.png" alt="" id="datasetLink" />
+                </a>
+            </div>
+            <span className={styles.weightedLine}> </span>
+        </div>
+    )
 }
 
 LabelTitle.propTypes = {
-  datasetName: PropTypes.string.isRequired,
-  datasetOrg: PropTypes.string.isRequired,
-  datasetNameLink: PropTypes.string.isRequired,
-  datasetOrgLink: PropTypes.string.isRequired
+    datasetName: PropTypes.string.isRequired,
+    datasetOrg: PropTypes.string.isRequired,
+    datasetNameLink: PropTypes.string.isRequired,
+    datasetOrgLink: PropTypes.string.isRequired
 }
 
 LabelTitle.defaultProps = {
-  datasetName: 'College Scoreboard Dataset 2020',
-  datasetOrg: 'US Department of Education',
-  datasetNameLink: '#',
-  datasetOrgLink: '#'
+    datasetName: 'College Scoreboard Dataset 2020',
+    datasetOrg: 'US Department of Education',
+    datasetNameLink: '#',
+    datasetOrgLink: '#'
 }
 
 export default LabelTitle
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,10 +1,7 @@
 import React from "react"
-import { Link } from "gatsby"
-import { Button } from "react-bootstrap"
+
 import LabelMenus from "../components/LabelMenus/index.js"
-import LabelTitle from "../components/LabelTitle/index.js"
 import Layout from "../components/layout"
-import SEO from "../components/seo"
 
 const SecondPage = () => (
     <Layout>
