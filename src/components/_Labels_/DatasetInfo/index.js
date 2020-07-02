e1e280087b1bd967686ba5ce66256edd3deafe06
Kemi Thomas <kemothom71@gmail.com>
2020-07-02 15:38:43 -0400

    changed DatasetInfo File

--- src/components/_Labels_/DatasetInfo/index.js
+++ src/components/_Labels_/DatasetInfo/index.js
@@ -1,13 +1,41 @@
 import React from "react"
-import SectionBase from "../../SectionBase/index.js"
+import { connect } from "react-redux"
+import styles from "./styles.module.css"
+import { fetchOverviewThunk } from "../../../store/overviewStore"
+
 class DatasetInfo extends React.Component {
   render() {
+    const overviewInfo = this.props.overview
+    const description = overviewInfo.description || []
     return (
-      <SectionBase>
-        <h1>DataSet Info</h1>
-      </SectionBase>
+      <div className={styles.overviewBase}>
+        <div className={styles.flexbox}>
+          <h1 className={styles.overviewTitle}>Overview</h1>
+          <span className={styles.overviewUnderlineBold}></span>
+        </div>
+        <h2 className={styles.overviewSubHeader}>Description</h2>
+        <ol className={styles.overviewList}>
+          {description.map(overview => (
+            <li>{overview.question}</li>
+          ))}
+        </ol>
+        <span className={styles.overviewUnderline}></span>
+        <h2 className={styles.overviewSubHeader}>Composition</h2>
+      </div>
     )
   }
 }
 
-export default DatasetInfo
+const mapStateToProps = state => {
+  return {
+    overview: state.overview,
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchOverview: dispatch(fetchOverviewThunk()),
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(DatasetInfo)
