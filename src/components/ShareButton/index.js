2af1ee1402a93094260375059ea969e6084b459f
Kemi Thomas <kemothom71@gmail.com>
2020-07-12 15:15:06 -0400

    further styled button with scss

--- src/components/ShareButton/index.js
+++ src/components/ShareButton/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Button from "react-bootstrap/Button"
-import styles from "./styles.module.css"
+import styles from "./styles.module.scss"
 
 const ShareButton = () => {
   return (
