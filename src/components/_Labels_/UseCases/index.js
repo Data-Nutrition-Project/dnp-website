82e121ba5fb4371e234574e5f5d69bf575bc85fa
Kemi Thomas <kemothom71@gmail.com>
2020-08-03 20:52:59 -0400

    html

--- src/components/_Labels_/UseCases/index.js
+++ src/components/_Labels_/UseCases/index.js
@@ -1,10 +1,11 @@
 import React from "react"
 import SectionBase from "../../SectionBase/index.js"
+import Alerts from "../../Alert/index.js"
 class UseCases extends React.Component {
   render() {
     return (
       <SectionBase>
-        <h1>Use Cases</h1>
+        <Alerts />
       </SectionBase>
     )
   }
