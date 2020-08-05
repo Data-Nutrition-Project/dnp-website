b36734c19452747be985f7a78c4d20a8d5453111
Matthew Taylor <mewtaylor@gmail.com>
2020-08-05 13:33:48 -0400

    Trying again

--- src/components/_tests_/header.test.js
+++ src/components/_tests_/header.test.js
@@ -1,6 +1,6 @@
 import React from "react"
 import renderer from "react-test-renderer"
-import Header from "../Header/index"
+import Header from "../Header"
 
 describe("Header", () => {
   it("renders correctly", () => {
