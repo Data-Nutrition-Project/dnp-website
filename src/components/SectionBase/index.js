b86533231a34eb1005da018b9987c584580df8c6
Matthew Taylor <mewtaylor@gmail.com>
2020-08-05 19:53:33 -0400

    Make label menu be fixed when scrolling
    There are a few refactors of styling here that I needed to do to make the fixed position work, especially with tablet view too

--- src/components/SectionBase/index.js
+++ src/components/SectionBase/index.js
@@ -1,8 +1,22 @@
 import React from "react"
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
 
 import styles from "./styles.module.css"
+
 const SectionBase = ({ children }) => {
-  return <div className={styles.sectionBase}>{children}</div>
+  return (
+    <Container>
+        <Col
+            className={styles.sectionBase}
+            xl={{ span: 12, offset: 2 }}
+            md={{ span: 9, offset: 3 }}
+            sm={{ span: 12 }}
+        >
+            {children}
+        </Col>
+    </Container>
+)
 }
 
 export default SectionBase
