ba7df2a5b6ffca7e36cb6facdfe3dc163521ded3
Matthew Taylor <mewtaylor@gmail.com>
2020-08-12 18:14:16 -0400

    Take 2 - use gatsby env variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,8 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
-    }
+    },
+    `gatsby-env-variables`
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
