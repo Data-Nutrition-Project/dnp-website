9b9dc788d4f04239dcae16881d1dff0499a1e9c2
hgm-king <hgmaxwellking@gmail.com>
2022-02-13 15:44:58 +0100

    got ourselves a database method to connect to our database locally and hopefully externally

---
+++ database/connector.js
@@ -0,0 +1,16 @@
+const { MongoClient } = require("mongodb");
+
+exports.connect = async (uri) => {
+  const client = new MongoClient(uri);
+  try {
+    // Connect the client to the server
+    await client.connect();
+    // Establish and verify connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Connected successfully to server");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+  return client;
+}
