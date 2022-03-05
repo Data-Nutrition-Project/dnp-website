551b313932aa7cfa2c725976de44431684971b20
hgm-king <hgmaxwellking@gmail.com>
2022-03-05 18:44:26 +0100

    new questionnaire controller and the tests

---
+++ controllers/questionnaire.js
@@ -0,0 +1,38 @@
+const { v4: uuidv4 } = require('uuid');
+
+class QuestionnaireController {
+  constructor(questionnaireService, templateService) {
+    this.questionnaireService = questionnaireService
+    this.templateService = templateService
+  }
+
+  // start a new questionnaire from the given templateId
+  async getQuestionnaireFromTemplate(templateId) {
+    const emptyTemplate = await this.templateService.getTemplate(templateId)
+    const questionnaireInserted = await this.questionnaireService.addQuestionnaire(emptyTemplate)
+
+    // we want both a mongo id and a dnp id
+    emptyTemplate._id = questionnaireInserted.insertedId
+    emptyTemplate.dnpId = uuidv4()
+
+    return emptyTemplate
+  }
+
+  // save a questionnaire as a user fills out new questions
+  async saveQuestionnaire(questionnaireObject) {
+    if ( questionnaireObject._id ) {
+      delete questionnaireObject._id
+    }
+
+    if ( questionnaireObject.schema_version ) {
+      questionnaireObject.schema_version += 1
+    } else {
+      questionnaireObject.schema_version = 2
+    }
+
+    const questionnaireResult = await this.questionnaireService.addQuestionnaire(questionnaireObject)
+    return questionnaireResult
+  }
+}
+
+exports.QuestionnaireController = QuestionnaireController
