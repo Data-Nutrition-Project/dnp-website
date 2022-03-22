018284f0302461c3ac2067e2b4573728a5054d70
hgm-king <hgmaxwellking@gmail.com>
2022-03-22 12:42:24 +0100

    good looking comments and documentation

--- routes/questionnaire.js
+++ routes/questionnaire.js
@@ -1,6 +1,15 @@
 const { ObjectID } = require('mongodb')
 
 exports.QuestionnairesRouter = (app, questionnaireController, questionnaireService) => {
+  /*
+  @params
+    questionnaireObject :: body - questionnaire that gets inserted into database
+  @desc
+    This route will accept a questionnaire through a POST body and insert it into
+      the database after bumping the schema_version
+  @return
+    insertedObject :: shaped like { id: _id, schema_version }
+  */
   app.post('/questionnaire', async (req, res) => {
     try {
       const savedQuestionnaire = await questionnaireController.saveQuestionnaire(req.body)
@@ -17,6 +26,16 @@ exports.QuestionnairesRouter = (app, questionnaireController, questionnaireServi
     }
   })
 
+  /*
+  @params
+    templateId :: queryParams - id of the template that you want to initialize
+  @desc
+    This route will accept a template id and will create and return an empty
+      questionnaire object
+  @return
+    questionnaire :: empty questoinnaire, will have no schema_version which means
+      it is new and empty
+  */
   app.get('/new-questionnaire', async (req, res) => {
     try {
       const emptyTemplate = await questionnaireController.getQuestionnaireFromTemplate(new ObjectID(req.query.id))
@@ -37,6 +56,15 @@ exports.QuestionnairesRouter = (app, questionnaireController, questionnaireServi
     }
   })
 
+  /*
+  @params
+    dnpId :: queryParams - dnp id of a questionnaire to be gotten
+  @desc
+    This route will accept a dnp id of a questionnaire and return the highest
+      version that is available in the database
+  @return
+    questionnaire :: questionnaire that has the given dnpId and the highest version
+  */
   app.get('/questionnaire', async (req, res) => {
     try {
       const foundQuestionnaire = await questionnaireService.getNewestQuestionnaire(req.query.id)
