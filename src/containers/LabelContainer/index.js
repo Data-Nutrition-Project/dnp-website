57b58af4f6a9a76b1d795c0f436b9d0bd8850429
Matthew Taylor <mewtaylor@gmail.com>
2023-04-10 11:38:22 -0400

    Set metadata for labels

--- src/containers/LabelContainer/index.js
+++ src/containers/LabelContainer/index.js
@@ -3,6 +3,7 @@ import classNames from "classnames"
 import { formatBlobForLabel } from "../../utils/jsonFormatter.js"
 import { formatBlobForLabel2 } from "../../utils/jsonFormatter2.js"
 import { Grid } from  'react-loader-spinner'
+import Helmet from "react-helmet"
 import Linkify from 'react-linkify';
 import React, { useState, useEffect } from "react"
 import PropTypes from "prop-types"
@@ -21,12 +22,15 @@ const LabelContainer = (props) => {
     const [labelBlob, setLabelBlob] = useState(null)
     const [error, setError] = useState(null)
     const [loading, setLoading] = useState(true)
+    const [title, setTitle] = useState("Dataset Nutrition Label v3")
+    const [keywords, setKeywords] = useState("")
+    const [version, setVersion] = useState(-1)
 
-    const getLabelWrapper = (labelBlob, loading) => {
-        if (labelBlob.version === 2) {
+    const getLabelWrapper = (labelBlob, loading, version) => {
+        if (version === 2) {
             return (
                 <LabelWrapper2 
-                    labelBlob={formatBlobForLabel2(labelBlob)}
+                    labelBlob={labelBlob}
                     loading={loading}
                     status={labelBlob.status}
                 />
@@ -34,18 +38,36 @@ const LabelContainer = (props) => {
         } else {
             return (
                 <LabelWrapper
-                    labelBlob={formatBlobForLabel(labelBlob)}
+                    labelBlob={labelBlob}
                     loading={loading}
                 />
             )
         }
     }
 
+    const setDataFromVersion = (data) => {
+        let blob = null
+        setVersion(data.version)
+        if (data.version === 2) {
+            blob = formatBlobForLabel2(data)
+            setLabelBlob(blob)
+            const keywords = blob.description.keywordList.map((obj) => {
+                return obj.Keyword.answer
+            })
+            setKeywords(keywords.join(","))
+        } else {
+            blob = formatBlobForLabel(data)
+            setLabelBlob(blob)
+            setKeywords(blob.description.keywordList.join(","))
+        }
+        setTitle(blob.title)
+    }
+
     const getLabelBlob = () => {
         axios
             .get(`${process.env.GATSBY_BACKEND_URL}/labels/${props.labelId}`)
             .then((response) => {
-                setLabelBlob(response.data)
+                setDataFromVersion(response.data)
             })
             .catch((err) => {
                 console.log(err)
@@ -74,46 +96,66 @@ const LabelContainer = (props) => {
     }
     
     return (
-        <div>
-        {loading ? (
+        <>
+            <Helmet
+                title={`Label for: ${title}`}
+                meta={[
+                    { 
+                        name: "description",
+                        content: "Dataset Nutrition Label describing the uses, risks and other basic information for a dataset."
+                    },
+                    {
+                        name: "keywords",
+                        content: "AI,Data,Quality".concat(",", keywords)
+                    },
+                ]}
+                bodyAttributes={{
+                  class: "stretched",
+                }}
+            >
+                <html lang="en" />
+            </Helmet>
             <div>
-                <LabelHeader loading={loading} />
-                <div className={styles.label}>
-                    <Row>
-                        <Col md={{ span: 4, offset: 4 }}>
-                            <Grid
-                                height="80"
-                                width="80"
-                                color={layout.darkSlateBlue}
-                                ariaLabel="grid-loading"
-                                radius="12.5"
-                                wrapperStyle={{}}
-                                wrapperClass=""
-                                visible={true}
-                            />
-                        </Col>
-                    </Row>
+            {loading ? (
+                <div>
+                    <LabelHeader loading={loading} />
+                    <div className={styles.label}>
+                        <Row>
+                            <Col md={{ span: 4, offset: 4 }}>
+                                <Grid
+                                    height="80"
+                                    width="80"
+                                    color={layout.darkSlateBlue}
+                                    ariaLabel="grid-loading"
+                                    radius="12.5"
+                                    wrapperStyle={{}}
+                                    wrapperClass=""
+                                    visible={true}
+                                />
+                            </Col>
+                        </Row>
+                    </div>
                 </div>
-            </div>
-        ) : error !== null ? (
-            <div>
-                <LabelHeader loading={true} />
-                <div className={styles.label}>
-                    <Row>
-                        <Col>
-                            <h1 style={{ 
-                                textAlign: 'center'
-                            }}>
-                                {'Sorry, something went wrong!'}
-                            </h1>
-                        </Col>
-                    </Row>
+            ) : error !== null ? (
+                <div>
+                    <LabelHeader loading={true} />
+                    <div className={styles.label}>
+                        <Row>
+                            <Col>
+                                <h1 style={{ 
+                                    textAlign: 'center'
+                                }}>
+                                    {'Sorry, something went wrong!'}
+                                </h1>
+                            </Col>
+                        </Row>
+                    </div>
                 </div>
-            </div>
-        ) : (
-            getLabelWrapper(labelBlob, loading)
-        )}
-        </div>   
+            ) : (
+                getLabelWrapper(labelBlob, loading, version)
+            )}
+            </div> 
+        </>  
     )
 }
 
