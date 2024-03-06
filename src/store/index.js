8fe72cb42a275626d02c1e4ea5847d6ccf64b119
Matthew Taylor <mewtaylor@gmail.com>
2024-03-05 21:38:42 -0500

    update dependencies

--- src/store/index.js
+++ src/store/index.js
@@ -1,24 +1,18 @@
-import { createStore, combineReducers, applyMiddleware } from "redux"
-import { createLogger } from "redux-logger"
-import thunkMiddleware from "redux-thunk"
-import { composeWithDevTools } from "redux-devtools-extension"
+import { configureStore } from "@reduxjs/toolkit"
 
 import labelMenuReducer from "./labelMenu"
 import dataReducer from "./labelBase"
 import baseReducer from "./bases"
 import labelReducer from "./labelStore"
 
-const reducer = combineReducers({
-  data: dataReducer,
-  labelMenu: labelMenuReducer,
-  base: baseReducer,
-  label: labelReducer,
+const store = configureStore({
+  reducer: {
+    data: dataReducer,
+    labelMenu: labelMenuReducer,
+    base: baseReducer,
+    label: labelReducer,
+  }
 })
-
-const middleware = composeWithDevTools(
-  applyMiddleware(thunkMiddleware, createLogger({ collapsed: true }))
-)
-const store = createStore(reducer, middleware)
 export default store
 
 export * from "./labelMenu"
